976ffddf0643ad508389092a42eb95717e33239f
Leon Chris <161019333+leonn05@users.noreply.github.com>
2024-09-25 19:53:54 +0530

    Add files via upload
    uploaded files

---
+++ main.py
@@ -0,0 +1,161 @@
+
+
+import cv2
+import numpy as np
+import mediapipe as mp
+import time
+import autopy
+from HandTrackingModule import handDetector
+
+##########################
+wCam, hCam = 1980, 1080
+frameR = 100  # Frame Reduction
+smoothening = 7
+#########################
+
+pTime = 0
+plocX, plocY = 0, 0
+clocX, clocY = 0, 0
+
+cap = cv2.VideoCapture(1)
+cap.set(3, wCam)
+cap.set(4, hCam)
+
+# Initialize hand detector
+detector = handDetector(maxHands=1)
+detector.set_detection_confidence(0.7)
+detector.set_tracking_confidence(0.5)
+
+wScr, hScr = autopy.screen.size()
+
+while True:
+    # 1. Find hand Landmarks
+    success, img = cap.read()
+    img = detector.findHands(img)
+    lmList, bbox = detector.findPosition(img)
+
+    # 2. Get the tip of the index and middle fingers
+    if lmList:
+        x1, y1 = lmList[8][1:]
+        x2, y2 = lmList[12][1:]
+
+        # 3. Check which fingers are up
+        fingers = detector.fingersUp()
+
+        # Draw a rectangle covering the entire camera frame
+        cv2.rectangle(img, (0, 0), (wCam, hCam), (255, 0, 255), 2)
+
+        # 4. Only Index Finger : Moving Mode
+        if fingers[1] == 1 and fingers[2] == 0:
+            # 5. Convert Coordinates
+            x3 = np.interp(x1, (frameR, wCam - frameR), (0, wScr))
+            y3 = np.interp(y1, (frameR, hCam - frameR), (0, hScr))
+
+            # 6. Smoothen Values
+            clocX = plocX + (x3 - plocX) / smoothening
+            clocY = plocY + (y3 - plocY) / smoothening
+
+            # 7. Move Mouse
+            autopy.mouse.move(wScr - clocX, clocY)
+            cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
+            plocX, plocY = clocX, clocY
+
+        # 8. Both Index and thumb fingers are pinching : Clicking Mode
+        if fingers[1] == 1 and fingers[4] == 1 and fingers[2] == 0 and fingers[3] == 0:  # Index finger and thumb finger
+            # 9. Find distance between fingers
+            length, img, lineInfo = detector.findDistance(8, 4, img)  # Index finger tip and thumb tip
+            print(length)
+            # 10. Click mouse if distance short
+            if length < 40:
+                cv2.circle(img, (lineInfo[4], lineInfo[5]), 15, (0, 255, 0), cv2.FILLED)
+                autopy.mouse.click()
+
+    # 11. Frame Rate
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+    cv2.putText(img, str(int(fps)), (20, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+
+    # 12. Display
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
+
+import cv2
+import numpy as np
+import mediapipe as mp
+import time
+import autopy
+from HandTrackingModule import handDetector
+
+##########################
+wCam, hCam = 1980, 1080
+frameR = 100  # Frame Reduction
+smoothening = 7
+#########################
+
+pTime = 0
+plocX, plocY = 0, 0
+clocX, clocY = 0, 0
+
+cap = cv2.VideoCapture(1)
+cap.set(3, wCam)
+cap.set(4, hCam)
+
+# Initialize hand detector
+detector = handDetector(maxHands=1)
+detector.set_detection_confidence(0.7)
+detector.set_tracking_confidence(0.5)
+
+wScr, hScr = autopy.screen.size()
+
+while True:
+    # 1. Find hand Landmarks
+    success, img = cap.read()
+    img = detector.findHands(img)
+    lmList, bbox = detector.findPosition(img)
+
+    # 2. Get the tip of the index and middle fingers
+    if lmList:
+        x1, y1 = lmList[8][1:]
+        x2, y2 = lmList[12][1:]
+
+        # 3. Check which fingers are up
+        fingers = detector.fingersUp()
+
+        # Draw a rectangle covering the entire camera frame
+        cv2.rectangle(img, (0, 0), (wCam, hCam), (255, 0, 255), 2)
+
+        # 4. Only Index Finger : Moving Mode
+        if fingers[1] == 1 and fingers[2] == 0:
+            # 5. Convert Coordinates
+            x3 = np.interp(x1, (frameR, wCam - frameR), (0, wScr))
+            y3 = np.interp(y1, (frameR, hCam - frameR), (0, hScr))
+
+            # 6. Smoothen Values
+            clocX = plocX + (x3 - plocX) / smoothening
+            clocY = plocY + (y3 - plocY) / smoothening
+
+            # 7. Move Mouse
+            autopy.mouse.move(wScr - clocX, clocY)
+            cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
+            plocX, plocY = clocX, clocY
+
+        # 8. Both Index and thumb fingers are pinching : Clicking Mode
+        if fingers[1] == 1 and fingers[4] == 1 and fingers[2] == 0 and fingers[3] == 0:  # Index finger and thumb finger
+            # 9. Find distance between fingers
+            length, img, lineInfo = detector.findDistance(8, 4, img)  # Index finger tip and thumb tip
+            print(length)
+            # 10. Click mouse if distance short
+            if length < 40:
+                cv2.circle(img, (lineInfo[4], lineInfo[5]), 15, (0, 255, 0), cv2.FILLED)
+                autopy.mouse.click()
+
+    # 11. Frame Rate
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+    cv2.putText(img, str(int(fps)), (20, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+
+    # 12. Display
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
